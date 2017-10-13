f028e5f2e598297f13ac582eaa7c1d130ad834d3
Giacomo Danese <Giacomo@ZingTasks-MacBook-Pro.local>
2017-10-12 19:13:57 -0700

    commit

--- server.js
+++ server.js
@@ -86,5 +86,5 @@ app.post("/api/new", function(req, res) {
 // Starts the server to begin listening
 // =============================================================
 app.listen(app.get('port'), function() {
-  console.log("App listening on PORT " + app.get('port');
+  console.log("App listening on PORT " + app.get('port'));
 });
